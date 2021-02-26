47c155265e1cb109c1185761904ef8c88f176ffc
Maicon <maicondouglasm19@gmail.com>
2021-02-26 09:23:39 -0300

    Conclusão do challenge box
    Concluído o challenge box e iniciado a criação dos contextos para fazer a comunicação entre os compontentes

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,8 +1,35 @@
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
+    const hasActiveChallenge = true;
+
     return (
         <div className={styles.challengeBoxContainer}>
+           {hasActiveChallenge ?( 
+               <div className={styles.challengeActive}>
+                   <header>Ganhe 400 xp</header>
+                   <main>
+                       <img src="icons/body.svg" alt=""/>
+                       <strong>Novo desafio</strong>
+                       <p>Levante e faça uma caminhada de 3 minutos.</p>
+                   </main>
+                   <footer>
+                       <button 
+                        type="button"
+                        className={styles.challengeFailedButton}
+                       >
+                           Falhei
+                        </button>
+                       <button 
+                        type="button"
+                        className={styles.challengeSucceededButton}
+
+                       >
+                           Completei
+                        </button>
+                   </footer>
+               </div>
+            ):(
             <div className={styles.challengeNotActive}>
                 <strong>Inicie um ciclo para receber desafios a serem completados</strong>
                 <p>
@@ -10,6 +37,7 @@ export function ChallengeBox() {
                     Avance de level completando desafios
                 </p>
             </div>
+            )}
         </div>
     );
 }
