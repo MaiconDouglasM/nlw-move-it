0d4e68a7597ffb5e6102fe13c86dce180a3d4649
Maicon <maicondouglasm19@gmail.com>
2021-02-26 11:06:58 -0300

    Finalizado os contextos
    Contexto de challenge completo, Contexto de coundown completo e notificação do navegador.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 
 export function Profile() {
+    const {level}=useContext(ChallengesContext);
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/kingurigu.png" alt="Maicon Douglas" />
@@ -9,7 +12,7 @@ export function Profile() {
                 <strong>Maicon Douglas</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
